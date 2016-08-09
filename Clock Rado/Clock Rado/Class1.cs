eea12543f2bf61a6163cb0a1c369f96e657988f4
vansx001 <vansx001@gmail.com>
2016-08-09 10:55:52 -0500

    Moving ClockRadio C# project

---
+++ Clock Rado/Clock Rado/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock_Rado
+{
+    public class Radio
+    {
+        public string station;
+        public bool isOn;
+        public int percentageVolume;
+
+        public Radio(string Station, bool IsOn, int PercentageVolume)
+        {
+            station = Station;
+            isOn = IsOn;
+            percentageVolume = PercentageVolume;
+        }
+        public void turnOn()
+        {
+            isOn = true;
+        }
+        public int TurnUp()
+        {
+            return percentageVolume += 5;
+        }
+    }
+}
---
+++ Clock Rado/Clock Rado/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock_Rado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio kissFM = new Radio("103.7", false, 100);
+            Radio v100 = new Radio("100.7", false, 100);
+            v100.station = "Jams";
+            kissFM.turnOn();
+            kissFM.TurnUp();
+        }
+    }
+}
